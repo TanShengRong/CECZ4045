14da3c1a7078ba8be4fcd248d9abccd09a97abdd
TanShengRong <tansr2010@gmail.com>
2020-10-12 15:15:20 +0800

    add scraper
    amazon reviews (choose the link with all reviews in form)

---
+++ amazon_reviews_scraping/amazon_reviews_scraping/spiders/amazon.py
@@ -0,0 +1,25 @@
+import scrapy
+
+
+class AmazonSpider(scrapy.Spider):
+    name = 'amazon'
+    # allowed_domains = ['https://www.amazon.com/product-reviews/B00YFTHJ9C/ref=acr_dp_hist_5?ie=UTF8&filterByStar=five_star&reviewerType=all_reviews#reviews-filter-bar']
+    myBaseUrl = "https://www.amazon.com/AmazonBasics-8-Sheet-Cross-Cut-Credit-Shredder/product-reviews/B00YFTHJ9C/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews&amp;amp;pageNumber="
+    start_urls = []
+    # Creating list of urls to be scraped by appending page number a the end of base url
+    for i in range(1,11):
+        start_urls.append(myBaseUrl+str(i))
+        
+    def parse(self, response):
+            data = response.css('#cm_cr-review_list')
+            # Collecting product star ratings
+            star_rating = data.css('.review-rating')
+            # Collecting user reviews
+            comments = data.css('.review-text')
+            count = 0
+            # Combining the results
+            for review in star_rating:
+                yield{'stars': ''.join(review.xpath('.//text()').extract()),
+                      'comment': ''.join(comments[count].xpath(".//text()").extract())
+                     }
+                count=count+1
