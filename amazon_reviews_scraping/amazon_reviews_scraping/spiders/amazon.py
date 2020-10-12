75370b5b1ed5c42f6c8a09b8cbafc127825ef50f
TanShengRong <tansr2010@gmail.com>
2020-10-12 21:29:45 +0800

    update ipynb to reflect corrected dataset

--- amazon_reviews_scraping/amazon_reviews_scraping/spiders/amazon.py
+++ amazon_reviews_scraping/amazon_reviews_scraping/spiders/amazon.py
@@ -4,10 +4,11 @@ import scrapy
 class AmazonSpider(scrapy.Spider):
     name = 'amazon'
     # allowed_domains = ['https://www.amazon.com/product-reviews/B00YFTHJ9C/ref=acr_dp_hist_5?ie=UTF8&filterByStar=five_star&reviewerType=all_reviews#reviews-filter-bar']
-    myBaseUrl = "https://www.amazon.com/AmazonBasics-8-Sheet-Cross-Cut-Credit-Shredder/product-reviews/B00YFTHJ9C/ref=cm_cr_dp_d_show_all_btm?ie=UTF8&reviewerType=all_reviews&amp;amp;pageNumber="
+    myBaseUrl = "https://www.amazon.com/AmazonBasics-8-Sheet-Cross-Cut-Credit-Shredder/product-reviews/B00YFTHJ9C/ref=cm_cr_arp_d_paging_btm_next_2?ie=UTF8&reviewerType=all_reviews&pageNumber="
+    
     start_urls = []
     # Creating list of urls to be scraped by appending page number a the end of base url
-    for i in range(1,11):
+    for i in range(1,101):
         start_urls.append(myBaseUrl+str(i))
         
     def parse(self, response):
@@ -20,6 +21,7 @@ class AmazonSpider(scrapy.Spider):
             # Combining the results
             for review in star_rating:
                 yield{'stars': ''.join(review.xpath('.//text()').extract()),
-                      'comment': ''.join(comments[count].xpath(".//text()").extract())
+                      'comment': ''.join(comments[count].xpath(".//text()").extract()).strip()
                      }
                 count=count+1
+            
