7b8e579a25dd0b9a4e16d815b1d2fbdbe5e6a08b
TanShengRong <tansr2010@gmail.com>
2020-10-23 22:39:19 +0800

    add aws comprehend example

---
+++ question_3_3/aws_sentiment.py
@@ -0,0 +1,28 @@
+# Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# This file is licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License. A copy of the
+# License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+# OF ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+
+
+import boto3
+import json
+
+comprehend = boto3.client(service_name='comprehend', region_name='region')
+                
+text = "It is raining today in Seattle"
+
+print('Calling DetectSentiment')
+print(json.dumps(comprehend.detect_sentiment(Text=text, LanguageCode='en'), sort_keys=True, indent=4))
+print('End of DetectSentiment\n')
+
+
+              
+  
+
