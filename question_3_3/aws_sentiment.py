13d719ae199b69c52d97230cf426f81b35effea3
TanShengRong <tansr2010@gmail.com>
2020-10-24 06:27:56 +0800

    add query using api

--- question_3_3/aws_sentiment.py
+++ question_3_3/aws_sentiment.py
@@ -1,26 +1,53 @@
-# Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-#
-# This file is licensed under the Apache License, Version 2.0 (the "License").
-# You may not use this file except in compliance with the License. A copy of the
-# License is located at
-#
-# http://aws.amazon.com/apache2.0/
-#
-# This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
-# OF ANY KIND, either express or implied. See the License for the specific
-# language governing permissions and limitations under the License.
+# CZ4045 Assignment 1
+# Question 3.3
 
-
-import boto3
 import json
-from botocore.config import Config
+import requests
+import pandas as pd
+import os.path
 
-comprehend = boto3.client(service_name='comprehend')
-text = "It is raining today in Seattle"
-# text = input("Please enter the text that should be parsed for sentiment analysis :")
 
-print('#=== Calling DetectSentiment ===#')
-response = comprehend.detect_sentiment(Text=text, LanguageCode='en')
-print(response)
-print('#=== End of DetectSentiment ===#')
-print("The sentence you provided has a sentiment of:", response['Sentiment'])
+class SentimentAnalysis:
+
+    url = "https://fhv4zgrdy0.execute-api.us-east-1.amazonaws.com/dev"
+
+    def __init__(self, df: pd.DataFrame):
+        self.df = df#.iloc[[0]]
+        self.sentiments = None
+
+    def query_sentiment(self):
+        self.sentiments = self.df["comment"].map(self.query_api)
+
+    def query_api(self, comment: str):
+        return True
+        payload = {
+            "text": comment
+        }
+        r = requests.post(self.url, json=payload)
+        r_dict = json.loads(r.text)
+        return r_dict['Sentiment']
+
+    def _print(self):
+        _df = pd.concat([self.df['stars'], self.df['comment'], self.sentiments], axis=1, keys=['Rating', 'Review', 'Sentiment'])
+        print(_df)
+
+
+if __name__ == "__main__":
+    print("""
+ ## ##   ### ##    ## ##   ##  ###  ### ##            ## ##     ## ###
+##   ##   ##  ##  ##   ##  ##   ##   ##  ##           ##  ##   ##   ##
+##        ##  ##  ##   ##  ##   ##   ##  ##               ##   ##
+##  ###   ## ##   ##   ##  ##   ##   ##  ##              ##    ## ###
+##   ##   ## ##   ##   ##  ##   ##   ## ##              ##     ##   ##
+##   ##   ##  ##  ##   ##  ##   ##   ##                #   ##  ##   ##
+ ## ##   #### ##   ## ##    ## ##   ####              ######    ## ##
+    """)
+    filepath = input("Enter name of file in current directory or its relative location: ")
+    # filepath = "30_reviews.csv"
+    if os.path.exists(filepath):
+        print("Classifying using AWS Comprehend...")
+        sa = SentimentAnalysis(pd.read_csv(filepath))
+        sa.query_sentiment()
+        sa._print()
+    else:
+        print("Please check filename and location")
