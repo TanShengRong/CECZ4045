e94581d6c6c1c4098ac4d3b7114250af43b47d14
Krishna <mkvyas999@gmail.com>
2020-11-02 02:13:30 +0800

    POS Tagger code with readme file.

---
+++ POS Tagger/POS_Tagger.py
@@ -0,0 +1,37 @@
+
+# # Pos tagger for Dataset linguistic characteristic analysis.
+
+
+from nltk import pos_tag,sent_tokenize,word_tokenize
+import glob
+import random
+import matplotlib.pyplot as plt
+
+## Read in the dataset files.
+files = glob.glob('*.txt')
+
+
+## Sentence segmentation for POS tagging each sentence. 
+print("Sentence segmentation performing...")
+sentences = []
+for f in files:
+    f = open(f, "r", encoding="utf-8")
+    text = f.read()
+    templist = sent_tokenize(text)
+    for item in templist:
+        sentences.append(item)
+print("segementation completed successfully!!")
+
+
+## select random sentences and apply POS tagging. 
+
+for i in range(3):
+    random_num = random.randint(0, len(sentences))
+    print(sentences[random_num])
+    print('\n')
+    print(pos_tag(word_tokenize(sentences[random_num])))
+    print('\n')
+
+
+
+
---
+++ POS Tagger/Readme.md
@@ -0,0 +1,10 @@
+# Libraries required
+1. nltk 
+
+# Installing required libraries
+1. pip install nltk
+
+# Running Pos tagger
+1. copy the POS_Tagger.py into the required dataset folder.
+
+2. run the POS_Tagger.py
